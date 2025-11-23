78bfc85e018566ac88a503e70d99dc0e60125886
axo <jacksonpope027@gmail.com>
2025-11-22 21:10:39 -0700

    First push 🤯

---
+++ FuckOffEars/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace FuckOffEars
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class Constants
+    {
+        public const string GUID = "author.FUCKOFFEARS";
+        public const string Name = "FUCKOFFEARS";
+        public const string Description = "Mod Description"; //The description is not needed but is nice to have
+        public const string Version = "1.0.0";
+    }
+}
---
+++ FuckOffEars/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BepInEx;
+
+namespace FuckOffEars
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+
+    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Start() => GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+
+        private void OnGameInitialized()
+        {
+            Console.Beep(32767, int.MaxValue);
+        }
+
+    }
+}
